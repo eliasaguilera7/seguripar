d032e038478f0154cd4c328df99f018cf211beca
Elias Aguilera Yambay <eliasaguilerayambay@Eliass-Mac-mini.local>
2026-03-04 09:42:27 -0300

    uploaded page to live

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/seguripar/',
+  base: '/',
   plugins: [
     tailwindcss(),
     react()],
